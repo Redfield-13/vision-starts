6c49580c9971e94598da041b8270e7f125e3dd0a
MJ redfield <fieldr62@gmail.com>
2021-09-02 05:56:52 +0200

    section img

---
+++ src/Components/sectionOne.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/sectionOne.css'
+
+function sectionOne() {
+    return (
+        <div className='sec-1'>
+            
+        </div>
+    )
+}
+
+export default sectionOne
